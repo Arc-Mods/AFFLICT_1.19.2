4f202305134149e60e61cc91534ba077fac3d9bf
LittlePuffGD <littlepuffgd@gmail.com>
2025-08-19 21:41:51 -0400

    bleh made more (no lang fuck you rn)

--- src/main/java/dev/arc/index/AfflictItems.java
+++ src/main/java/dev/arc/index/AfflictItems.java
@@ -2,15 +2,15 @@ package dev.arc.index;
 
 import dev.arc.item.CoinItem;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class AfflictItems {
 
     public static final Item COIN = register("coin", new CoinItem(new Item.Settings()));
+    public static final Item STAR_CORE = register("star_core", new Item(new Item.Settings()));
+    public static final Item BLADE = register("blade", new SwordItem(ToolMaterials.NETHERITE, 4, -2.7f, new Item.Settings()));
 
     private static Item register(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier("afflict", name), item);
@@ -21,6 +21,8 @@ public class AfflictItems {
             .icon(() -> new ItemStack(COIN))
             .appendItems(stacks -> {
                 stacks.add(new ItemStack(COIN));
+                stacks.add(new ItemStack(STAR_CORE));
+                stacks.add(new ItemStack(BLADE));
             })
             .build();
 
